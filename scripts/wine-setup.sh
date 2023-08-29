30059d24bc8820df5d075bf8885375ea86eab0de
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-08-28 21:02:18 -0400

    Update wine-setup.sh

--- scripts/wine-setup.sh
+++ scripts/wine-setup.sh
@@ -1,2 +1,8 @@
 pacman -S wine
 xdg-mime default wine.desktop exe
+cd /tmp/installer/
+mkdir winetricks
+cd winetricks
+wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+winetricks dxvk-master
