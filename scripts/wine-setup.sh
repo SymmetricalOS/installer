5c31231cd1da979032cfa158729e7613fe68a3db
Sid <saytleader@outlook.com>
2023-08-28 21:04:21 -0400

    Update wine-setup.sh

--- scripts/wine-setup.sh
+++ scripts/wine-setup.sh
@@ -1,8 +1,8 @@
-pacman -S wine
+pacman -S wine --no-confirm
 xdg-mime default wine.desktop exe
 cd /tmp/installer/
-mkdir winetricks
+mkdir -p winetricks
 cd winetricks
 wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
-chmod +x winetricks
-winetricks dxvk-master
+chmod +x ./winetricks
+./winetricks dxvk-master
