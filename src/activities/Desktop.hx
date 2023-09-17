64021491524ab4897208c171e568f8573a6abe16
Sid <saytleader@outlook.com>
2023-09-16 21:08:22 -0400

    Almost finish the UI

--- src/activities/Desktop.hx
+++ src/activities/Desktop.hx
@@ -7,6 +7,7 @@ import flixel.group.FlxGroup;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import ui.GProgress;
+import util.Config;
 import util.Progress;
 
 class Desktop extends FlxState {
@@ -17,6 +18,15 @@ class Desktop extends FlxState {
 	static var progress:FlxGroup;
 
 	static var button:FlxText;
+
+	static var wmtitle:FlxText;
+
+	static var opxfwm:FlxText;
+	static var opi3:FlxText;
+	static var opopenbox:FlxText;
+	static var opawesome:FlxText;
+
+	static var tiling:FlxText;
 
 	override public function create() {
 		super.create();
@@ -40,6 +50,40 @@ class Desktop extends FlxState {
 		button.x = FlxG.width - button.width;
 		button.y = FlxG.height - button.height;
 		add(button);
+
+		wmtitle = new FlxText(0, 0, 0, "Window Manager").setFormat("legato-sans.ttf", 48, FlxColor.BLACK);
+		wmtitle.screenCenter(X);
+		wmtitle.y = title.height * 2;
+		add(wmtitle);
+
+		opxfwm = new FlxText(0, 0, 0, "XFWM - The default look and feel of Symmetrical OS").setFormat("legato-sans.ttf", 32, FlxColor.fromInt(0xFFED820E));
+		opxfwm.screenCenter(X);
+		opxfwm.y = wmtitle.y + (wmtitle.height * 1.5);
+		add(opxfwm);
+
+		opi3 = new FlxText(0, 0, 0, "i3 - A lightweight tiling* manager for power users").setFormat("legato-sans.ttf", 32, FlxColor.BLACK);
+		opi3.screenCenter(X);
+		opi3.y = opxfwm.y + opxfwm.height;
+		add(opi3);
+
+		opopenbox = new FlxText(0, 0, 0, "Openbox - A basic but customizable window manager").setFormat("legato-sans.ttf", 32, FlxColor.BLACK);
+		opopenbox.screenCenter(X);
+		opopenbox.y = opi3.y + opi3.height;
+		add(opopenbox);
+
+		opawesome = new FlxText(0, 0, 0, "Awesome - A highly configurable tiling* window manager").setFormat("legato-sans.ttf", 32, FlxColor.BLACK);
+		opawesome.screenCenter(X);
+		opawesome.y = opopenbox.y + opopenbox.height;
+		add(opawesome);
+
+		tiling = new FlxText(0, 0, 0,
+			"*tiling window manager - A window manager where you use keyboard shortcuts to manage and open windows. These are usually for advanced users.")
+			.setFormat("legato-sans.ttf", 32, FlxColor.BLACK);
+		tiling.wordWrap = true;
+		tiling.width = FlxG.width / 1.5;
+		tiling.screenCenter(X);
+		tiling.y = FlxG.height - (title.height * 2);
+		add(tiling);
 	}
 
 	override public function update(elapsed:Float) {
@@ -47,5 +91,34 @@ class Desktop extends FlxState {
 
 		if (FlxG.mouse.overlaps(button) && FlxG.mouse.justPressed)
 			FlxG.switchState(new Confirm());
+
+		if (FlxG.mouse.overlaps(opxfwm) && FlxG.mouse.justPressed) {
+			opxfwm.color = FlxColor.fromInt(0xFFED820E);
+			opi3.color = FlxColor.BLACK;
+			opopenbox.color = FlxColor.BLACK;
+			opawesome.color = FlxColor.BLACK;
+			Config.DESKTOP_WM = "xfwm";
+		}
+		if (FlxG.mouse.overlaps(opi3) && FlxG.mouse.justPressed) {
+			opi3.color = FlxColor.fromInt(0xFFED820E);
+			opxfwm.color = FlxColor.BLACK;
+			opopenbox.color = FlxColor.BLACK;
+			opawesome.color = FlxColor.BLACK;
+			Config.DESKTOP_WM = "i3";
+		}
+		if (FlxG.mouse.overlaps(opopenbox) && FlxG.mouse.justPressed) {
+			opopenbox.color = FlxColor.fromInt(0xFFED820E);
+			opi3.color = FlxColor.BLACK;
+			opxfwm.color = FlxColor.BLACK;
+			opawesome.color = FlxColor.BLACK;
+			Config.DESKTOP_WM = "openbox";
+		}
+		if (FlxG.mouse.overlaps(opawesome) && FlxG.mouse.justPressed) {
+			opawesome.color = FlxColor.fromInt(0xFFED820E);
+			opi3.color = FlxColor.BLACK;
+			opopenbox.color = FlxColor.BLACK;
+			opxfwm.color = FlxColor.BLACK;
+			Config.DESKTOP_WM = "awesome";
+		}
 	}
 }
---
+++ src/util/Config.hx
@@ -0,0 +1,12 @@
+package util;
+
+class Config {
+	public static var LOCAL_PARTITION_DISK = "";
+	public static var LOCAL_PARTITION_LAYOUT_MAIN = "";
+	public static var LOCAL_PARTITION_LAYOUT_BOOT = "";
+
+	public static var USER_USERNAME = "";
+	public static var USER_PASSWORD = "";
+
+	public static var DESKTOP_WM = "xfwm";
+}
