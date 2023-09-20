d52304d78aa4d76d3161b65813528a4186bfe888
SidGames5 <saytleader@outlook.com>
2023-09-20 07:18:20 -0400

    Finish xfce script

--- scripts/xfce.sh
+++ scripts/xfce.sh
@@ -3,12 +3,16 @@
 pacman -S xorg xorg-xinit --noconfirm
 pacman -S xfce4 xfce4-goodies --noconfirm
 
-# cd /etc/installer/
-# git clone https://aur.archlinux.org/xfce4-docklike-plugin.git
-# cd xfce4-docklike-plugin/
-# makepkg -si --noconfirm
+cd /etc/installer
+git clone https://github.com/nsz32/docklike-plugin && cd docklike-plugin
+./autogen.sh
+make
+sudo make install
 
 # TODO: apply customizations
 
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 echo exec startxfce4 > ~/.xinitrc
+
+pacman -S lightdm --noconfirm
+systemctl enable lightdm.service
